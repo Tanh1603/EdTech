afa4b41a395ec346aad2db65b0fe8b728fd1f2b7
Tuananh <tuananh48218@gmail.com>
2026-05-07 18:09:30 +0700

    Add Swagger documentation setup for EdTech API
    - Integrated Swagger for API documentation, enhancing developer experience and API usability.
    - Configured Swagger with title, version, and authentication settings.
    - Established a dedicated route for accessing API documentation at '/api/docs'.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
@@ -12,6 +13,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder().setTitle("EdTech api")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addGlobalResponse().build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
